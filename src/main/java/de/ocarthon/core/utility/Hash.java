99076c57afee9285aec741ea45a0d5ecb28db9db
Philip Standt <philip.standt@gmail.com>
2015-05-05 19:49:56 +0200

    [Utility] Hash-Class for easy access to default hash functions

---
+++ src/main/java/de/ocarthon/core/utility/Hash.java
@@ -0,0 +1,103 @@
+/*
+ *    Copyright 2015 Ocarthon (Philip Standt)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package de.ocarthon.core.utility;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * A Utility-class providing methods for various hashing
+ * algorithms
+ */
+public class Hash {
+
+
+    /**
+     * hashes the input using the given algorithm. It is not possible to revert
+     * this process.
+     *
+     * @param algorithm the name of the algorithm
+     * @param input     the bytes that should we hashed
+     * @return the hash of the input
+     * @throws java.lang.NullPointerException if the algorithm or the
+     *                                        input is null
+     */
+    public static byte[] hash(String algorithm, byte[] input) {
+        if (algorithm == null) throw new NullPointerException("algorithm must be set");
+        if (input == null) throw new NullPointerException("input must be set");
+
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            md.update(input);
+            return md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    /**
+     * hashes the input using the MD5-Algorithm. It is not possible to revert
+     * this process. The MD5-Algorithm is a standard algorithm and is always
+     * implemented
+     *
+     * @param input the bytes that should we hashed
+     * @return the hash of the input
+     * @throws java.lang.NullPointerException if the input is null
+     */
+    public static byte[] md5(byte[] input) {
+        return hash("MD5", input);
+    }
+
+    /**
+     * hashes the input using the SHA1-Algorithm. It is not possible to revert
+     * this process. The SHA1-Algorithm is a standard algorithm and is always
+     * implemented
+     *
+     * @param input the bytes that should we hashed
+     * @return the hash of the input
+     * @throws java.lang.NullPointerException if the input is null
+     */
+    public static byte[] sha1(byte[] input) {
+        return hash("SHA-1", input);
+    }
+
+    /**
+     * hashes the input using the SHA256-Algorithm. It is not possible to revert
+     * this process. The SHA256-Algorithm is a standard algorithm and is always
+     * implemented
+     *
+     * @param input the bytes that should we hashed
+     * @return the hash of the input
+     * @throws java.lang.NullPointerException if the input is null
+     */
+    public static byte[] sha256(byte[] input) {
+        return hash("SHA-256", input);
+    }
+
+    /**
+     * hashes the input using the SHA512-Algorithm. It is not possible to revert
+     * this process. The SHA512-Algorithm is not a standard algorithm and must
+     * not be implemented on every system
+     *
+     * @param input the bytes that should we hashed
+     * @return the hash of the input
+     * @throws java.lang.NullPointerException if the input is null
+     */
+    public static byte[] sha512(byte[] input) {
+        return hash("SHA-512", input);
+    }
+}
---
+++ src/test/java/de/ocarthon/core/utility/HashTest.java
@@ -0,0 +1,80 @@
+/*
+ *    Copyright 2015 Ocarthon (Philip Standt)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package de.ocarthon.core.utility;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNull;
+
+public class HashTest {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private byte[] data = "LoremIpsum".getBytes();
+
+    @Test
+    public void testHash() throws Exception {
+        // only for coverage
+        new Hash();
+    }
+
+    @Test
+    public void testHashAlgorithmNPE() throws Exception {
+        exception.expect(NullPointerException.class);
+        Hash.hash(null, data);
+    }
+
+    @Test
+    public void testHashDataNPE() throws Exception {
+        exception.expect(NullPointerException.class);
+        Hash.hash("SHA-128", null);
+    }
+
+    @Test
+    public void testHashNoSuchAlgorithm() {
+        assertNull(Hash.hash("NonExistingAlgorithm", data));
+    }
+
+    @Test
+    public void testMd5() throws Exception {
+        assertArrayEquals(Hex.toHexString(Hash.md5(data)).getBytes(),
+                "d8acb9272677b376f85fde36b8a3e762".toLowerCase().getBytes());
+    }
+
+    @Test
+    public void testSha() throws Exception {
+        assertArrayEquals(Hex.toHexString(Hash.sha1(data)).getBytes(),
+                "37de7433db93c025a175cc1417dcada8fff85366".toLowerCase().getBytes());
+    }
+
+    @Test
+    public void testSha256() throws Exception {
+        assertArrayEquals(Hex.toHexString(Hash.sha256(data)).getBytes(),
+                ("8b4d4fda010ef93e765dea76b3ec0b5fa33a96a4d5c5973f27a03b" +
+                        "d33272599f").toLowerCase().getBytes());
+    }
+
+    @Test
+    public void testSha512() throws Exception {
+        assertArrayEquals(Hex.toHexString(Hash.sha512(data)).getBytes(),
+                ("d78e22a2b89c2f1a7e467e3254fdd2db7df5d66730a261fab62f2b0" +
+                        "95109ba53a017fbdf5ad856d7ca938d0f37787d18fd375ec" +
+                        "90619b7709f0baca091db4b51").toLowerCase().getBytes());
+    }
+}
